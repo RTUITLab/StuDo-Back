1f2814aeeb1b084eb4d2d36a5a80bc5a2e2365a7
Nick <nikitamm59rus@gmail.com>
2019-07-30 16:23:30 +0300

    Changes in confirm email html to let make replace for normal url

--- studo/Services/EmailSender.cs
+++ studo/Services/EmailSender.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Net.Mail;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
@@ -22,7 +23,9 @@ namespace studo.Services
 
         public async Task SendEmailConfirmationAsync(string email, string redirectUrl)
         {
-            var message = $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(redirectUrl)}'>clicking here</a>.";
+            //var message = $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(redirectUrl)}'>clicking here</a>.";
+            var message = (await GetConfirmEmailTemplateAsync())
+                .Replace("%url%", $"{HtmlEncoder.Default.Encode(redirectUrl)}");
             await SendEmailAsync(email, "Confirm your email", message);
         }
 
@@ -32,7 +35,7 @@ namespace studo.Services
             await SendEmailAsync(email, "Reset password", message);
         }
 
-        public async Task SendEmailAsync(string email, string subject, string message)
+        private async Task SendEmailAsync(string email, string subject, string message)
         {
             MailMessage mailMessage = new MailMessage
             {
@@ -53,5 +56,8 @@ namespace studo.Services
 
             await smtpClient.SendMailAsync(mailMessage);
         }
+
+        private Task<string> GetConfirmEmailTemplateAsync()
+            => new HttpClient().GetStringAsync(options.ConfirmEmailTemplateUrl);
     }
 }
