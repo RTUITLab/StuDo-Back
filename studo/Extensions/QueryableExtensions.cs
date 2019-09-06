e4f9cc2b1fd70d3aa9c470fa76f72093cd88f6ba
Nick <nikitamm59rus@gmail.com>
2019-09-06 20:53:14 +0300

    Get organizations where user can publish or is a member

---
+++ studo/Extensions/QueryableExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace studo.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> IfNotNull<T, TS> (
+            this IQueryable<T> queryable,
+            TS? checkObj,
+            Func<IQueryable<T>, IQueryable<T>> ifNotNullQueryable) where TS : struct
+            => If(queryable, checkObj.HasValue, ifNotNullQueryable);
+
+        public static IQueryable<T> If<T>(
+            this IQueryable<T> queryable,
+            bool check,
+            Func<IQueryable<T>, IQueryable<T>> ifTrueQueryable)
+            => check ? ifTrueQueryable(queryable) : queryable;
+    }
+}
