c6400fdc823d69df28a3c18179a6c03231fe1e02
Nick <nikitamm59rus@gmail.com>
2019-09-07 09:55:56 +0300

    Add userId and orgId to compact ad view

--- studo/Models/Responses/Ads/CompactAdView.cs
+++ studo/Models/Responses/Ads/CompactAdView.cs
@@ -11,7 +11,10 @@ namespace studo.Models.Responses.Ads
         public DateTime BeginTime { get; set; }
         public DateTime EndTime { get; set; }
 
+        public Guid? UserId { get; set; }
         public string UserName { get; set; }
+
+        public Guid? OrganizationId { get; set; }
         public string OrganizationName { get; set; }
     }
 }
